d222365fad0035fdeffbb53f816d3d7d727ba736
RoneyGajjar <roneygajjar121@gmail.com>
2024-09-25 16:56:50 +0530

    Add files via upload

---
+++ Assignments/Module 3/Basic Syntax/Q1.c
@@ -0,0 +1,17 @@
+/*1. Display This Information using printf
+a. Your Name
+b. Your Birth date
+c. Your Age
+d. Your Address
+*/
+
+#include<stdio.h>
+int main()
+{
+	printf("\nName = Roney Gajjar ");
+	printf("\nDate Of Birth = 30 March 2002");
+	printf("\nAge = 22Years ");
+	printf("\nAddress = Ahmedabad ");
+	
+	return 0;
+}

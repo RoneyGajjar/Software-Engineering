2ba68f0a4dd9cd757297d43b3015b955584a258d
RoneyGajjar <roneygajjar121@gmail.com>
2024-09-25 17:41:50 +0530

    Add files via upload

---
+++ Assignments/Module 3/Looping Statment/Q7.c
@@ -0,0 +1,21 @@
+//7. WAP to print number in reverse order e.g.: number = 64728 ---> reverse = 82746
+#include <stdio.h>
+int main()
+{
+
+	int i,num,rem=0,rev ;
+	
+	printf("Enter the number :");
+	scanf("%d",&num);
+	
+	for( ; num != 0 ;)
+	{
+		rem = num %10;
+		rev = (rev*10)+rem;
+		num = num/10;
+	}
+	
+	printf("\n Reversed number = %d",rev);
+	
+	return 0;
+}
